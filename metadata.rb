305096ad712bbe111ccb26ac393489c0065acd93
cullenmcdermott <cullenmcd@gmail.com>
2017-01-21 16:29:48 -0600

    test

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cullenmcd@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.6'
 
 supports 'centos', '~> 7.1'
 supports 'ubuntu', '~> 14.04'
